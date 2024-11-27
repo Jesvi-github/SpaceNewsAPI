0af3cca17d2fd480f35ba48749d6db12b592202b
Jesbin-CMPG <200588911@student.georgianc.on.ca>
2024-11-27 14:39:03 -0500

    added javafx and style properties

--- src/main/java/com/spacenews/spacenews/SpaceNewsApi.java
+++ src/main/java/com/spacenews/spacenews/SpaceNewsApi.java
@@ -3,15 +3,27 @@ package com.spacenews.spacenews;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class SpaceNewsApi extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/spacenews/spacenews/hello-view.fxml"));
         Scene scene = new Scene(loader.load());
+
+        // Set the title of the window
         primaryStage.setTitle("Space News");
+
+        // Set the app icon (make sure the image path is correct)
+        primaryStage.getIcons().add(new Image("file:src/main/resources/com/spacenews/spacenews/icon.jpeg"));
+
+        // Set the scene
         primaryStage.setScene(scene);
+
+        // Show the window
         primaryStage.show();
     }
 
